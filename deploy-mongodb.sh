93f5901c691fca110263eb4c811b1e5d526101d9
pjuvigny <pierre.juvigny@capgemini.com>
2015-06-22 10:01:10 +0200

    Added push to registry

--- deploy-mongodb.sh
+++ deploy-mongodb.sh
@@ -7,3 +7,8 @@ sudo docker rm db
 sudo docker build -t pjuvigny/mongodb docker-mongodb
 
 sudo docker run -p 127.0.0.1:27000:27017 -d --name db pjuvigny/mongodb
+
+
+sudo docker tag pjuvigny/mongodb localhost:5000/mongodb:$1
+sudo docker tag -f pjuvigny/mongodb localhost:5000/mongodb:latest
+sudo docker push localhost:5000/mongodb
--- deploy-node.sh
+++ deploy-node.sh
@@ -7,3 +7,7 @@ sudo docker rm nodejs
 sudo docker build -t pjuvigny/node-test docker-nodejs
 
 sudo docker run -p 49110:8000 -d --name nodejs --link db:db pjuvigny/node-test
+
+sudo docker tag pjuvigny/node-test localhost:5000/node-test:$1
+sudo docker tag -f pjuvigny/node-test localhost:5000/node-test:latest
+sudo docker push localhost:5000/node-test
